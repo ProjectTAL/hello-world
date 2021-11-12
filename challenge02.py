7b2bf30a3287970b0fe478b56b6101a2e2c8bfb1
현유석/B2B개발그룹(무선)/Staff Engineer/삼성전자 <yuseok.hyun@samsung.com>
2021-11-12 21:04:48 +0900

    resolved for 013

--- challenge02.py
+++ challenge02.py
@@ -14,6 +14,17 @@ def cmpNumbers(n1, n2):
         print(f"{n2}, then {n1}")
     else:
         print(f"{n1}, then {n2}")
+#013 사용자에게 20보다 작은 숫자를 입력하라고 요청한다. 만약 입력된 값이 20과 같거나 크면 "Too high"라는 메시지를 출력하라.
+#    그렇지 않다면 "Thank you"를 출력하라.
+def getNumberLessthan20():
+    ret = int(input("Input the number that you want but MUST be less than 20 : "))
+    return ret
+def decideCondition(num):
+    if num >= 20:
+        print("Too high")
+    else:
+        print("Thank you")
 if __name__ == '__main__':
     testPrint()
-    cmpNumbers(getNumber1(), getNumber2())
+    decideCondition(getNumberLessthan20())
+    #012 cmpNumbers(getNumber1(), getNumber2())
