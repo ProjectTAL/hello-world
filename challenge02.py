37d97cfd6f9ec0725f7d4bca1f4adf5eda399f93
ethan <squashno76@gmail.com>
2021-11-12 22:05:11 +0900

    fixed for 013 and 014

--- challenge02.py
+++ challenge02.py
@@ -14,6 +14,26 @@ def cmpNumbers(n1, n2):
         print(f"{n2}, then {n1}")
     else:
         print(f"{n1}, then {n2}")
+#013
+def getNumberLessthan20():
+    ret = int(input("Put the number less than 20 : "))
+    return ret
+def decideCondition01(num):
+    if num >= 20:
+        print("Too High")
+    else:
+        print("Thank you")
+#014
+def getNumberooo():
+    ret = int(input("Put the number 10-20 : "))
+    return ret
+def decideCondition02(num):
+    if num >=10 and num <= 20:
+        print("Thank you")
+    else:
+        print("Incorrect answer")
 if __name__ == '__main__':
     testPrint()
-    cmpNumbers(getNumber1(), getNumber2())
+    decideCondition02(getNumberooo())
+    #013 decideCondition01(getNumberLessthan20())
+    #012 cmpNumbers(getNumber1(), getNumber2())
