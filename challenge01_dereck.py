dcefd17293c887e7c487fd444a72d9148b1e1321
Derek <tomorrow6pack@naver.com>
2021-11-27 15:54:43 +0900

    resolved for 001

---
+++ challenge01_dereck.py
@@ -0,0 +1,7 @@
+
+def print_hello(name):
+    print(f'hello, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+if __name__ == '__main__':
+    textValue = input("Enter a text value: ")
+    print_hello(textValue)
