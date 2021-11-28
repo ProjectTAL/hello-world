714c42123b93df21748788e7df764f49bda65e98
Derek <tomorrow6pack@naver.com>
2021-11-28 17:09:34 +0900

    Resolved for 013

--- challenge02_derek.py
+++ challenge02_derek.py
@@ -1,13 +1,20 @@
 if __name__ == '__main__':
 
-    # 012
-    numValue = int(input("Enter the first number: "))
-    numValue2 = int(input("Enter the second number: "))
-    if numValue > numValue2:
-        print(numValue)
+    # 013
+    numValue = int(input("Enter a number less than 20: "))
+    if numValue >= 20:
+        print("Too high")
     else:
-        numValue2 > numValue
-        print(numValue2)
+        print("Thank you")
+
+    # 012
+    # numValue = int(input("Enter the first number: "))
+    # numValue2 = int(input("Enter the second number: "))
+    # if numValue > numValue2:
+    #     print(numValue)
+    # else:
+    #     numValue2 > numValue
+    #     print(numValue2)
 
     # 011
     # numValue = int(input("Enter a number above 100: "))
