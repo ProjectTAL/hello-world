6cd1127689c2e3c9625e3add700ad529930c0e9a
Derek <tomorrow6pack@naver.com>
2021-11-28 17:01:18 +0900

    Resolved 012

---
+++ challenge02_derek.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__':
+
+    # 012
+    numValue = int(input("Enter the first number: "))
+    numValue2 = int(input("Enter the second number: "))
+    if numValue > numValue2:
+        print(numValue)
+    else:
+        numValue2 > numValue
+        print(numValue2)
+
+    # 011
+    # numValue = int(input("Enter a number above 100: "))
+    # numValue2 = int(input("Please enter a number below 10: "))
+    # print(f' {numValue}goes {numValue2}in {numValue / numValue2}in. ')
