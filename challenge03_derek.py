308f2843fadbcd4c7752262342e6287848037c7a
Derek <tomorrow6pack@naver.com>
2021-12-01 20:54:08 +0900

    Resolved for 021

--- challenge03_derek.py
+++ challenge03_derek.py
@@ -1,7 +1,14 @@
 import astropy
 
 if __name__ == '__main__':
+    # 021
+    Name = input("Enter your name: ")
+    Lastname = input("Enter your last name: ")
+    Linearcontent = len(Name)
+    Linearcontent2 = len(Lastname)
+    print(Name, Lastname, Linearcontent + Linearcontent2)
+
     # 020
-    nameValue = input("Enter your name: ")
-    name = len(nameValue)
-    print(name)
+    # nameValue = input("Enter your name: ")
+    # name = len(nameValue)
+    # print(name)
