f66143ae210c98cd88f2b46880111d9bcb6005c6
Derek <tomorrow6pack@naver.com>
2021-12-01 20:59:22 +0900

    Resolved for 022

--- challenge03_derek.py
+++ challenge03_derek.py
@@ -1,12 +1,17 @@
 import astropy
 
 if __name__ == '__main__':
+    # 022
+    Name = input("Enter your name in lowercase: ")
+    Name2 = input("Enter your last name in lowercase: ")
+    print(Name.capitalize(), Name2.capitalize())
+
     # 021
-    Name = input("Enter your name: ")
-    Lastname = input("Enter your last name: ")
-    Linearcontent = len(Name)
-    Linearcontent2 = len(Lastname)
-    print(Name, Lastname, Linearcontent + Linearcontent2)
+    # Name = input("Enter your name: ")
+    # Lastname = input("Enter your last name: ")
+    # Linearcontent = len(Name)
+    # Linearcontent2 = len(Lastname)
+    # print(Name, Lastname, Linearcontent + Linearcontent2)
 
     # 020
     # nameValue = input("Enter your name: ")
