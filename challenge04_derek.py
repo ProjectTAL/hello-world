ccbfa2b56391cdaaa46a8785fe454757062d544b
Derek <tomorrow6pack@naver.com>
2021-12-04 10:56:06 +0900

    Resolved for 027

--- challenge04_derek.py
+++ challenge04_derek.py
@@ -1,10 +1,4 @@
 if __name__ == '__main__':
     # 026
-    PigLatin = input("Enter the word: ")
-    PigLatinlength = len(PigLatin)
-    PigLatin1 = PigLatin[1: PigLatinlength]
-    PigLatin2 = PigLatin[0]
-    if PigLatin[0] == "a" or PigLatin[0] == "e" or PigLatin[0] == "i" or PigLatin[0] == "o" or PigLatin[0] == "u":
-        print(PigLatin + "way")
-    else:
-        print(PigLatin1 + PigLatin2 + "ay")
+    num = float(input("Please enter a number with many decimal places: "))
+    print(num * 2)
