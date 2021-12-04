546d3714cfc542c4e7df844024d83834a0c18758
Derek <tomorrow6pack@naver.com>
2021-12-04 11:00:16 +0900

    Resolved for 028

--- challenge04_derek.py
+++ challenge04_derek.py
@@ -1,4 +1,8 @@
 if __name__ == '__main__':
+    # 028
+    num = float(input("Enter the number: "))
+    print(round(num, 2))
+
     # 026
-    num = float(input("Please enter a number with many decimal places: "))
-    print(num * 2)
+    # num = float(input("Please enter a number with many decimal places: "))
+    # print(num * 2)
