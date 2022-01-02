161326275c253d3d97164758e372308d70b298fa
Derek <tomorrow6pack@naver.com>
2022-01-02 15:34:25 +0900

    Resolved for 096-104

---
+++ challenge12_derek.py
@@ -0,0 +1,111 @@
+# 104
+# name1 = input("Enter the name")
+# age1 = int(input("Enter the age"))
+# size1 = int(input("Enter the shoe size"))
+#
+# name2 = input("Enter the name")
+# age2 = int(input("Enter the age"))
+# size2 = int(input("Enter the shoe size"))
+#
+# name3 = input("Enter the name")
+# age3 = int(input("Enter the age"))
+# size3 = int(input("Enter the shoe size"))
+#
+# name4 = input("Enter the name")
+# age4 = int(input("Enter the age"))
+# size4 = int(input("Enter the shoe size"))
+#
+# data_set = {name1: {"name": name1, "age": age1}, name2: {"name": name2, "age": age2},
+#             name3: {"name": name3, "age": age3}, name4: {"name": name4, "age": age4}}
+# delite = input("Enter the name")
+# del data_set[delite]
+# print(data_set)
+
+
+# 103
+# name1 = input("Enter the name")
+# age1 = int(input("Enter the age"))
+#1
+# name2 = input("Enter the name")
+# age2 = int(input("Enter the age"))
+#
+# name3 = input("Enter the name")
+# age3 = int(input("Enter the age"))
+#
+# name4 = input("Enter the name")
+# age4 = int(input("Enter the age"))
+#
+# data_set = {name1: {"name": name1, "age": age1}, name2: {"name": name2, "age": age2},
+#             name3: {"name": name3, "age": age3}, name4: {"name": name4, "age": age4}}
+# print(data_set)
+
+
+# 102
+# name1 = input("Enter the name")
+# age1 = int(input("Enter the age"))
+# size1 = int(input("Enter the shoe size"))
+#
+# name2 = input("Enter the name")
+# age2 = int(input("Enter the age"))
+# size2 = int(input("Enter the shoe size"))
+#
+# name3 = input("Enter the name")
+# age3 = int(input("Enter the age"))
+# size3 = int(input("Enter the shoe size"))
+#
+# name4 = input("Enter the name")
+# age4 = int(input("Enter the age"))
+# size4 = int(input("Enter the shoe size"))
+#
+# data_set = {name1: {"name": name1, "age": age1, "size": size1}, name3: {"name": name2, "age": age2, "size": size2},
+#             name3: {"name": name3, "age": age3, "size": size3}, name4: {"name": name4, "age": age4, "size": size4}}
+# name = input("Enter the name: ")
+# if name == name1:
+#     print(data_set[name1])
+# if name == name2:
+#     print(data_set[name2])
+# if name == name3:
+#     print(data_set[name3])
+# if name == name4:
+#     print(data_set[name4])
+
+# 101
+# data_set = {"john": {"N": 3056, "S": 8463, "E": 8441, "W": 2694}, "Tom": {"N": 4832, "S": 6786, "E": 4737, "W": 3612},
+#             "Anne": {"N": 5238, "S": 4802, "E": 5820, "W": 1859}, "Fiona": {"N": 3904, "S": 3645, "E": 8821, "W": 2451}}
+# name = input("Enter the name: ")
+# area = input("Enter the area: ")
+# print(data_set[name][area])
+
+
+# 100
+# data_set = {"john": {"N": 3056, "S": 8463, "E": 8441, "W": 2694}, "Tom": {"N": 4832, "S": 6786, "E": 4737, "W": 3612},
+#             "Anne": {"N": 5238, "S": 4802, "E": 5820, "W": 1859}, "Fiona": {"N": 3904, "S": 3645, "E": 8821, "W": 2451}}
+
+# 099
+# simple_array = [[2, 5, 8], [3, 7, 4], [1, 6, 9], [4, 2, 0]]
+# row = int(input("Enter the row: "))
+# print(simple_array[row])
+# column = int(input("Enter the column: "))
+# print(simple_array[row][column])
+# change = input("Do you want to change?(y or n): ")
+# if change == "y":
+#     new = int(input("Enter the number"))
+#     simple_array[row][column] = new
+# print(simple_array[row])
+
+# 098
+# simple_array = [[2, 5, 8], [3, 7, 4], [1, 6, 9], [4, 2, 0]]
+# row = int(input("Enter the row: "))
+# print(simple_array[row])
+# answer = int(input("Enter the number"))
+# simple_array[row].append(answer)
+# print(simple_array)
+
+# 097
+# simple_array = [[2, 5, 8], [3, 7, 4], [1, 6, 9], [4, 2, 0]]
+# row = int(input("Enter the row: "))
+# column = int(input("Enter the column: "))
+# print(simple_array[row][column])
+
+# 096
+# simple_array = [[2, 5, 8], [3, 7, 4], [1, 6, 9], [4, 2, 0]]
