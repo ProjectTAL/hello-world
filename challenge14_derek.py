06e7cd9334bce2a9a825fb1a05a0d8bc2ed30fba
Derek <tomorrow6pack@naver.com>
2022-01-03 11:13:23 +0900

    Resolved for 111-114

---
+++ challenge14_derek.py
@@ -0,0 +1,55 @@
+import csv
+
+# 114
+
+
+# 113
+# file = open("Books.csv", "w")
+# new = data_set = {"0": {"제목": "To Kill A Mockingbird", "저자": "Harper Lee", "출간연도": 1960},
+#                   "1": {"제목": "A Brief History of Time", "저자": "Stephen Hawking", "출간연도": 1988},
+#                   "2": {"제목": "The Great Gatsby", "저자": "F.Scott Fizgerald", "출간연도": 1922},
+#                   "3": {"제목": "The Man Who Mistook His Wife for a hat", "저자": "Olver Sacks", "출간연도": 1985},
+#                   "4": {"제목": "Pride and Prejudice", "저자": "Jane Austen", "출간연도": 1813}}
+# file.close()
+# num = int(input("Enter the number you want to add: "))
+# for i in range(0, num):
+#     file = open("Books.csv", "w")
+#     name = input("Enter a books name")
+#     author = input("Enter a books name")
+#     year = int(input("Enter the Year of books"))
+#     data = name + "," + author + "," + year + "\n"
+#     file.write(str(data))
+# file.close()
+# file = open("Books.csv", "r")
+# search = input("Enter the data you are searching for: ")
+# for row in file:
+#     if search in str(row):
+#         print(row)
+#     else:
+#         print("There is no data you are looking for")
+
+
+# 112
+# file = open("Books.csv", "w")
+# new = data_set = {"0": {"제목": "To Kill A Mockingbird", "저자": "Harper Lee", "출간연도": 1960},
+#                   "1": {"제목": "A Brief History of Time", "저자": "Stephen Hawking", "출간연도": 1988},
+#                   "2": {"제목": "The Great Gatsby", "저자": "F.Scott Fizgerald", "출간연도": 1922},
+#                   "3": {"제목": "The Man Who Mistook His Wife for a hat", "저자": "Olver Sacks", "출간연도": 1985},
+#                   "4": {"제목": "Pride and Prejudice", "저자": "Jane Austen", "출간연도": 1813}}
+# file.close()
+# file = open("Books.csv", "w")
+# name = input("Enter a books name")
+# author = input("Enter a books name")
+# year = int(input("Enter the Year of books"))
+# data = name + "," + author + "," + year + "\n"
+# file.write(str(data))
+# file.close()
+
+# 111
+# file = open("Books.csv", "w")
+# new = data_set = {"0": {"제목": "To Kill A Mockingbird", "저자": "Harper Lee", "출간연도": 1960},
+#                   "1": {"제목": "A Brief History of Time", "저자": "Stephen Hawking", "출간연도": 1988},
+#                   "2": {"제목": "The Great Gatsby", "저자": "F.Scott Fizgerald", "출간연도": 1922},
+#                   "3": {"제목": "The Man Who Mistook His Wife for a hat", "저자": "Olver Sacks", "출간연도": 1985},
+#                   "4": {"제목": "Pride and Prejudice", "저자": "Jane Austen", "출간연도": 1813}}
+# file.close()
